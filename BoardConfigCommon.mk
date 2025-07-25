07d81cad00835dcbe7da5fdf4a8dbd64897701e2
dianlujitao <dianlujitao@lineageos.org>
2025-07-25 20:15:59 +0800

    sm8450-common: Set force_sysfs_fallback=1 sysctl via cmdline
    early-init class gets triggered after all recovery modules are already
    loaded, which prevents ADSP firmware from being loaded.
    
    Change-Id: I4eaf2890bd98a6a11b6a87ad7330827fbf9b44d7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,11 +91,12 @@ BOARD_VENDOR_RAMDISK_FRAGMENT.dlkm.KERNEL_MODULE_DIRS := top
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
+    disable_dma32=on \
+    sysctl.kernel.firmware_config.force_sysfs_fallback=1 \
     mtdoops.fingerprint=$(LINEAGE_VERSION) \
     allow_file_spec_access \
     irqaffinity=0-3 \
     pelt=8
-
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
